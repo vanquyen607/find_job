274216f88ed8d8cd880388ca7689df9ae913dcc6
vanquyen607 <vanquyen607@gmail.com>
2026-08-28 09:08:11 +0700

    Fix CSP: add scriptSrcAttr for inline event handlers

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:", "https:"],
